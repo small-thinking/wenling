ca9fc41ea1cc63c7b87b4665d400bf3ee292543d
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2023-11-30 16:30:43 -0800

    Add utils

---
+++ wenling/tests/common/test_utils.py
@@ -0,0 +1,28 @@
+"""Test the utils.
+Run test: poetry run pytest tests/common/test_utils.py
+"""
+import pytest
+from wenling.common.utils import *
+from unittest.mock import patch
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [(1681684094, "2023-04-16 14:28:14")],
+)
+def test_get_datetime(input, expected):
+    assert get_datetime(input) == expected
+
+
+@pytest.mark.parametrize(
+    "url, status_code, expected",
+    [
+        ("https://example.com", 200, True),
+        ("https://example.com", 404, False),
+        ("https://example.invalid", None, False),
+    ],
+)
+def test_check_url_exists(url, status_code, expected):
+    with patch("requests.head") as mock_head:
+        mock_head.return_value.status_code = status_code
+        result = check_url_exists(url)
+        assert result == expected
